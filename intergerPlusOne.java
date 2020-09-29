262763bcce50b667bb969cea5ebb53e0f817a078
meghappatel24 <71676491+meghappatel24@users.noreply.github.com>
2020-09-28 20:14:13 -0400

    Add files via upload

---
+++ intergerPlusOne.java
@@ -0,0 +1,12 @@
+package Assignment;
+
+public class intergerPlusOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=36;
+		System.out.println(a+1);
+
+}
+
+}
---
+++ sumOfTwoIntegers.java
@@ -0,0 +1,16 @@
+package Assignment;
+
+public class sumOfTwoIntegers {
+
+	public static void main(String[] args) {
+		int a=19;
+		int b=12;
+		
+		System.out.println("Sum of 2 Integers a and b is");		
+		 System.out.println("a is 19 and b is 12");
+		
+		System.out.print("sum = ");
+		System.out.print(a+b);
+	}
+
+}
